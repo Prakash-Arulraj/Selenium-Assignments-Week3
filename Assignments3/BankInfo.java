93e9f95aed8b455250f19dd6963d8c4e0ed43e69
Prakash-Arulraj <82603756+Prakash-Arulraj@users.noreply.github.com>
2021-05-04 23:44:33 +0530

    Update BankInfo.java

--- Assignments3/BankInfo.java
+++ Assignments3/BankInfo.java
@@ -1,4 +1,16 @@
 package week3.day1.Assignments3;
+/* assignment 3:
+
+class : bankinfo
+methods : saving(), fixed(),deposit()
+
+create another one class
+class : axisbank
+methods : deposit()
+
+description:
+you have to override the method deposit in axisbank.
+*/
 
 public class BankInfo {
 	public void saving() {
