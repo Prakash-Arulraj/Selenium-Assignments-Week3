22ea0ddacb7cd7874c6cb6d42092eb7acaf92b21
Prakash-Arulraj <82603756+Prakash-Arulraj@users.noreply.github.com>
2021-05-04 23:33:47 +0530

    Update Computer.java

--- org/system/Computer.java
+++ org/system/Computer.java
@@ -1,4 +1,17 @@
 package week3.day1.Assignments1.org.system;
+/* assignment 1:
+package : computer
+class : computer
+methods : computermodel()
+
+create another one class
+class : desktop
+methods : desktopsize()
+
+description:
+create above 2 class and call all your class methods into the desktop using single inheritance
+
+*/
 
 public class Computer {
 	
