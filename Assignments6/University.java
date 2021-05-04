6b32f2b96db034a6858951adc54ad81fa9bf99ee
Prakash-Arulraj <82603756+Prakash-Arulraj@users.noreply.github.com>
2021-05-04 23:56:08 +0530

    Update University.java

--- Assignments6/University.java
+++ Assignments6/University.java
@@ -1,4 +1,14 @@
 package week3.day1.Assignments6;
+/* assignment 6:
+abstractclass : university
+methods :pg()(implimented method),ug(unimplimented method).
+
+class : college extends university
+methods : ug()
+description:
+ug() is just a template in university abstract class and you have to implement the method ug() in college class. execute both ug() & pg().
+*/
+
 
 public abstract class University {
 	public void pg() {
