992ed099c6cafebc17f6358e3637c2810784fe5c
Prakash-Arulraj <82603756+Prakash-Arulraj@users.noreply.github.com>
2021-05-04 21:43:23 +0530

    Add files via upload

---
+++ org/system/Computer.java
@@ -0,0 +1,18 @@
+package week3.day1.Assignments1.org.system;
+
+public class Computer {
+	
+	public void ComputerModel() {
+		System.out.println("enter the computer model");
+		
+
+	}
+	public void price() {
+		System.out.println("enter the price");
+		
+
+	}
+
+	
+
+}
---
+++ org/system/Desktop.java
@@ -0,0 +1,18 @@
+package week3.day1.Assignments1.org.system;
+
+public class Desktop extends Computer {
+	public void desktopsize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		//Desktop system = new Desktop();
+		//system.desktopsize();           
+		Computer system = new Computer();
+		system.ComputerModel();                
+		
+
+	}
+
+}
